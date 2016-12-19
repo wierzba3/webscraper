ad37ceb4929fdd79441f11a007b07856e6879892
James Wierzba <jwierzba@communityinvestorsinc.com>
2016-12-19 00:10:06 -0700

    - add email regex

--- src/com/jameswierzba/webscraper/WebScraper.java
+++ src/com/jameswierzba/webscraper/WebScraper.java
@@ -2,6 +2,8 @@ package com.jameswierzba.webscraper;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.helper.Validate;
@@ -12,8 +14,13 @@ import org.jsoup.select.Elements;
 public class WebScraper {
 
 	private static String SAMPLE_HTML = "";
-	//private static String URL_BASE = "http://www.bing.com/search?q=cleaning+services";
+
 	private static String URL_BASE = "http://www.bing.com/search?q=";
+	
+	private static final String EMAIL_REGEX =
+			"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 	
 	public static void main(String[] args) throws IOException 
 	{
@@ -40,10 +47,9 @@ public class WebScraper {
         	resultUrls.add(resultUrl);
         }
         
-        
-        //TODO for each url, navigate to page and search for email regex
-        
-        
+        //TODO loop through each result url and scan for email using regex
+        String html = "...result page html...";
+        Matcher matcher = EMAIL_PATTERN.matcher(html);
 	}
 
 }
