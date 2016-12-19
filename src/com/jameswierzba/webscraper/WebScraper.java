4798b0bf7abbb58b0aa34336095d9227aa7d7660
James Wierzba <jwierzba@communityinvestorsinc.com>
2016-12-18 22:43:39 -0700

    - initial commit

---
+++ src/com/jameswierzba/webscraper/WebScraper.java
@@ -0,0 +1,13 @@
+package com.jameswierzba.webscraper;
+
+public class WebScraper {
+
+	private static String SAMPLE_HTML = "";
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
